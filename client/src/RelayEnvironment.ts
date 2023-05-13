fd5b2ab278042d4e27dd18c14feff72cc234c778
Matt Peebles <peebles.mm@gmail.com>
2023-05-13 13:22:00 -0700

    one more time

--- client/src/RelayEnvironment.ts
+++ client/src/RelayEnvironment.ts
@@ -6,7 +6,7 @@ import {
   FetchFunction,
 } from "relay-runtime";
 
-const HTTP_ENDPOINT = "https://eurovision.azurewebsites.net/";
+const HTTP_ENDPOINT = "https://eurovision.azurewebsites.net/graphql";
 const fetchFn: FetchFunction = async (request, variables) => {
   const resp = await fetch(HTTP_ENDPOINT, {
     method: "POST",
