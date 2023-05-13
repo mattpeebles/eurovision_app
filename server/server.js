bef6f2803dfbafbf7ff799ac749531832ad61540
Matt Peebles <peebles.mm@gmail.com>
2023-05-13 11:19:49 -0700

    add basic paths

--- server/server.js
+++ server/server.js
@@ -2,17 +2,57 @@ var express = require("express")
 var { graphqlHTTP } = require("express-graphql")
 var { buildSchema } = require("graphql")
 
+const fake_db = {
+  'User': {
+    '1': {
+      first_name: "Matt",
+      last_name: "Peebles"
+    }
+  },
+  'Song': {
+    
+  }
+}
+
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
-    hello: String
+    me: User
+    songs: [Song]
+    rankings: [Ranking]
+  }
+  type User {
+    id: String
+    first_name: String
+    last_name: String
+  }
+  type Country {
+    id: String
+    name: String
+  }
+  type Song {
+    id: String
+    country: Country
+    artist: String
+    title: String
+    year: Int
+  }
+  type Score{
+    user: User
+    points: Int
+    song: Song
+  }
+  type Ranking {
+    song: Song
+    total: Int
+    scores: Score
   }
 `)
 
 // The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return "Hello world!"
+  me: () => {
+    return fake_db['User']['1'];
   },
 }
 
